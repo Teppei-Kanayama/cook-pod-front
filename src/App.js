cd36c04f2bc408ac909378a7abda43cdb3f4596b
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-01-23 20:40:09 +0900

    separate files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
+import NewDish from './pages/NewDish';
+import Dishes from './pages/Dishes'
 
 import {
   BrowserRouter as Router,
@@ -35,56 +36,4 @@ export default function App() {
       </div>
     </Router>
   );
-}
-
-function Dishes() {
-  return <h2>料理一覧</h2>;
-}
-
-class NewDish extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event, func) {
-    func(this.state.value)
-    event.preventDefault();  // よくわからないが、これがないと空欄に戻る。（APIは叩かれる）
-  }
-
-  render() {
-    const craeteDish = async (dishName) => {
-      try {
-        const result = await axios.post(
-          'http://localhost:3000/dishes',
-          {
-            name: dishName,
-            url: 'http://example.com/test',
-            memo: '特になし'
-          }
-        );
-        console.log(result);
-      } catch (error) {
-        console.log('error :/');
-      }
-    };
-
-    // TODO: URLやメモも送信する
-    return (
-      <form onSubmit={(event) => {this.handleSubmit(event, craeteDish)}}>
-        <label>
-          料理名:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="送信" />
-      </form>
-    );
-  }
 }
---
+++ src/pages/Dishes.js
@@ -0,0 +1,5 @@
+function Dishes() {
+  return <h2>料理一覧</h2>;
+}
+
+export default Dishes;
