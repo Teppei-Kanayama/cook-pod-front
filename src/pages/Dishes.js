203eba2cbbc2f6aa9515378dca487070ad08dcb5
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-01-24 14:31:46 +0900

    play with hook

--- src/pages/Dishes.js
+++ src/pages/Dishes.js
@@ -1,5 +1,34 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+
 function Dishes() {
-  return <h2>料理一覧</h2>;
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(100);  // the current state value and a function that lets you update it
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count - 100)}>
+        Click me
+      </button>
+
+      <HomeButton />
+    </div>
+  );
+}
+
+function HomeButton() {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push("/record");
+  }
+
+  return (
+    <button type="button" onClick={handleClick}>
+      Go home
+    </button>
+  );
 }
 
 export default Dishes;
