e04871a7cff10fa731086cd5c847e41c7455f27d
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-01-24 20:41:24 +0900

    fetch data on click

--- src/pages/Dishes.js
+++ src/pages/Dishes.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
 function Dishes() {
-  return <h2>料理の一覧</h2>;
+  const apiUrl = 'http://localhost:3000/dishes';
+
+  const fetchData = async() => {
+    const response = await axios.get(apiUrl)
+    console.log(response);
+  }
+
+  return (
+    <div>
+      <h2>料理の一覧</h2>
+      <button onClick={fetchData}>
+          表示
+      </button>
+    </div>
+  );
 }
 
 export default Dishes;
