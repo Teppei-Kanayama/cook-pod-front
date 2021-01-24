42bbd96a4fd0882a8d3218500b68b21708e2fbb1
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-01-24 22:41:22 +0900

    poor popup

--- src/pages/Dishes.js
+++ src/pages/Dishes.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Dishes.css';
+
+class PopUp extends React.Component {
+  handleClick = () => {
+   this.props.toggle();
+  };
+render() {
+  return (
+   <div className="modal">
+     <div className="modal_content">
+     <span className="close" onClick={this.handleClick}>&times;    </span>
+     <p>I'm A Pop Up!!!</p>
+    </div>
+   </div>
+  );
+ }
+}
 
 function Dish(props) {
+  const [seen, setSeen] = useState(false);
 
-  const onClick = () => {
-    console.log(props.dishId);
-  }
+  const togglePop = () => {
+    console.log('clicked');
+    setSeen(!seen);
+  };
+
+  // const onClick = () => {
+  //   console.log(props.dishId);
+  // }
 
   return (
       <div>
-        <button className="square" onClick={ onClick }>
-          <p>{ props.name }</p>
-          <p>{ props.url }</p>
-          <p>{ props.memo }</p>
-        </button>
+        <div>
+          <button className="square" onClick={ togglePop }>
+            <p>{ props.name }</p>
+            <p>{ props.url }</p>
+            <p>{ props.memo }</p>
+          </button>
+        </div>
+        {seen ? <PopUp toggle={ togglePop } /> : null}
       </div>
   );
 }
