0723963010605d311939de340af5b13b54828286
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-01-26 08:52:23 +0900

    refacotr

--- src/pages/NewDish.js
+++ src/pages/NewDish.js
@@ -2,68 +2,82 @@ import React from 'react';
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 
-function SubmitForm(props) {
+function NewDishForm(props) {
   let history = useHistory();
 
-  function handleSubmit(onSubmit) {
-    onSubmit();
-    history.push("/");
-  }
-
-  const craeteDish = async (dishName) => {
+  const newDish = async (name, url, memo) => {
     try {
       const result = await axios.post(
         'http://localhost:3000/dishes',
         {
-          name: dishName,
-          url: 'http://example.com/test',
-          memo: '特になし'
+          name: name,
+          url: url,
+          memo: memo
         }
       );
       console.log(result);
     } catch (error) {
-      console.log('何かがおかしい');
+      console.log('Error!');
     }
   };
 
-  // TODO: URLやメモも送信する
-  // TODO: リファクタ
+  function handleSubmit(event) {
+    newDish(props.formNameValue, props.formUrlValue, props.formMemoValue);
+    event.preventDefault();  // これは何？
+    history.push("/");  // 料理詳細ページを作ったら、そこに飛ぶように変更する
+  }
+
   return (
-    <form onSubmit={(event) => {handleSubmit( () => {props.onSubmit(event, craeteDish)})}}>
-    <label>
-    料理名:
-    <input type="text" value={props.value} onChange={props.handleChange} />
-    </label>
-    <input type="submit" value="送信" />
+    <form onSubmit={ handleSubmit }>
+      <p>料理名 <input type="text" value={ props.formNameValue } onChange={ props.handleNameChange }/></p>
+      <p>URL <input type="text" value={ props.formUrlValue } onChange={ props.handleUrlChange }/></p>
+      <p>メモ <input type="text" value={ props.formMemoValue } onChange={ props.handleMemoChange }/></p>
+      <input type="submit" value="送信" />
     </form>
-    );
+  );
 }
 
 class NewDish extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      formNameValue: '',
+      formUrlValue: '',
+      formMemoValue: '',
+    };
+    // これらは何？
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.handleMemoChange = this.handleMemoChange.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  // TODO: 要素ごとに関数を作っているのをなんとかしたい
+  handleNameChange(event) {
+    this.setState({formNameValue: event.target.value});
   }
 
-  handleSubmit(event, onSubmit) {
-    onSubmit(this.state.value)
-    event.preventDefault();  // よくわからないが、これがないと空欄に戻る。（APIは叩かれる）
+  handleUrlChange(event) {
+    this.setState({formUrlValue: event.target.value});
+  }
+
+  handleMemoChange(event) {
+    this.setState({formMemoValue: event.target.value});
   }
 
   render() {
     return (
-      <SubmitForm
-        onSubmit={ this.handleSubmit }
-        handleChange = {this.handleChange}
-        value = {this.value}
-      />
-    );
+      <div>
+        <h2>料理の新規登録</h2>
+        <NewDishForm
+          formNameValue={ this.state.formNameValue }
+          formUrlValue={ this.state.formUrlValue }
+          formMemoValue={ this.state.formMemoValue }
+          handleNameChange={ this.handleNameChange }
+          handleUrlChange={ this.handleUrlChange }
+          handleMemoChange={ this.handleMemoChange }
+        />
+      </div>
+    )
   }
 }
 
