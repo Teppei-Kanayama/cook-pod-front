324ff0d0b6041e18d5bf09d1dce18c8baf6f7583
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-01-25 23:07:23 +0900

    good

--- src/pages/EditDish.js
+++ src/pages/EditDish.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 import {
-  useParams
+  useParams,
+  useHistory
 } from "react-router-dom";
+
+function FormSubmission(props) {
+  let history = useHistory();
+
+  function handleSubmit(event) {
+    // ここで関数定義をするのは一般的なのか？
+    const editDish = async (dishId, name, url, memo) => {
+      try {
+        const result = await axios.put(
+          'http://localhost:3000/dishes/' + dishId,
+          {
+            name: name,
+            url: url,
+            memo: memo
+          }
+        );
+        console.log(result);
+      } catch (error) {
+        console.log('何かがおかしい');
+      }
+    };
+    editDish(props.dishId, props.formNameValue, props.formUrlValue, props.formMemoValue);
+    event.preventDefault();
+    history.push("/");
+  }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <p>料理名 <input type="text" value={ props.formNameValue } onChange={ props.handleNameChange }/></p>
+      <p>URL <input type="text" value={ props.formUrlValue } onChange={ props.handleUrlChange }/></p>
+      <p>メモ <input type="text" value={ props.formMemoValue } onChange={ props.handleMemoChange }/></p>
+      <input type="submit" value="送信" />
+    </form>
+  );
+}
 
 class EditDishForm extends React.Component {
   constructor(props) {
@@ -16,7 +52,7 @@ class EditDishForm extends React.Component {
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleUrlChange = this.handleUrlChange.bind(this);
     this.handleMemoChange = this.handleMemoChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -30,6 +66,7 @@ class EditDishForm extends React.Component {
       });
   }
 
+  // TODO: 要素ごとに関数を作っているのをなんとかしたい
   handleNameChange(event) {
     this.setState({formNameValue: event.target.value});
   }
@@ -42,41 +79,40 @@ class EditDishForm extends React.Component {
     this.setState({formMemoValue: event.target.value});
   }
 
-  handleSubmit(event) {
-    // ここで関数定義をするのは一般的なのか？
-    const editDish = async (name, url, memo) => {
-      try {
-        const result = await axios.put(
-          'http://localhost:3000/dishes/' + this.props.dishId,
-          {
-            name: name,
-            url: url,
-            memo: memo
-          }
-        );
-        console.log(result);
-      } catch (error) {
-        console.log('何かがおかしい');
-      }
-    };
-    editDish(this.state.formNameValue, this.state.formUrlValue, this.state.formMemoValue);
-    event.preventDefault();
-  }
-
-  handleSubmitButton(evnet) {
-    console.log('pushed!')
-  }
+  // handleSubmit(event) {
+  //   // ここで関数定義をするのは一般的なのか？
+  //   const editDish = async (name, url, memo) => {
+  //     try {
+  //       const result = await axios.put(
+  //         'http://localhost:3000/dishes/' + this.props.dishId,
+  //         {
+  //           name: name,
+  //           url: url,
+  //           memo: memo
+  //         }
+  //       );
+  //       console.log(result);
+  //     } catch (error) {
+  //       console.log('何かがおかしい');
+  //     }
+  //   };
+  //   editDish(this.state.formNameValue, this.state.formUrlValue, this.state.formMemoValue);
+  //   event.preventDefault();
+  // }
 
   render() {
     return (
       <div>
         <h2>料理の編集</h2>
-        <form onSubmit={ this.handleSubmit }>
-          <p>料理名 <input type="text" value={ this.state.formNameValue } onChange={ this.handleNameChange }/></p>
-          <p>URL <input type="text" value={ this.state.formUrlValue } onChange={ this.handleUrlChange }/></p>
-          <p>メモ <input type="text" value={ this.state.formMemoValue } onChange={ this.handleMemoChange }/></p>
-          <input type="submit" value="送信" onClick={ this.handleSubmitButton }/>
-        </form>
+        <FormSubmission
+          dishId={ this.props.dishId }
+          formNameValue={ this.state.formNameValue }
+          formUrlValue={ this.state.formUrlValue }
+          formMemoValue={ this.state.formMemoValue }
+          handleNameChange={ this.handleNameChange }
+          handleUrlChange={ this.handleUrlChange }
+          handleMemoChange={ this.handleMemoChange }
+        />
       </div>
     )
   }
