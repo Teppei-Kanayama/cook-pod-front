c3eedcd5cfb3bb6dc4b3da43b75650023fc2740a
Teppei Kanayama <kanayama.teppei@gmail.com>
2021-02-07 17:48:08 +0900

    example

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,5 +1,21 @@
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+
 function Example() {
-  return <h2>例</h2>;
+  const fetchData = async() => {
+    try {
+      const response = await axios.get('http://10.0.1.207/');
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  return (
+    <Button onClick={fetchData}>
+        表示
+    </Button>
+  );
 }
 
 export default Example;
